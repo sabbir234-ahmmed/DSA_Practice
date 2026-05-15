5ab90891ae16eb2f77bac4f9b8790665f4e6fd83
SABBIR_AHMMED_SAKIB <sh350084@gmail.com>
2026-05-16 05:04:40 +0600

    Optimize minnimumInSortedRoatedArrayHard problem

--- dailyLeetcodeProblem/minimumInSortedRoatedArrayHard.cpp
+++ dailyLeetcodeProblem/minimumInSortedRoatedArrayHard.cpp
@@ -1,11 +1,45 @@
+// class Solution {
+// public:
+//     int findMin(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+
+//         int mini=nums[0];
+
+//         return mini;
+//     }
+// };
+
+
+
+//for interview purpose , or interviewer purpose accoding to problem what exactly expected
+
 class Solution {
+    public:
+     int binarySerarch(int l, int r, vector<int>& nums){
+        
+
+        while(1){
+            if(l==r){
+                return nums[l];
+            }
+
+            int mid= (r-l)/2+l;
+
+            if(nums[mid]<nums[r]){
+                r=mid;
+            }else if(nums[mid]>nums[r]){
+                l=mid+1;
+            }else if(nums[mid]==nums[r]){
+                r--;
+            }
+        }
+     }
 public:
     int findMin(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int n= nums.size();
+       
+       int mini = binarySerarch(0,n-1,nums);
 
-        int mini=nums[0];
-
-        return mini;
+       return mini;
     }
 };
-
